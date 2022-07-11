5ba93f49d6be43e9989cf2bbc40ad562bbd5dac3
Fabio <fabiodanille@hotmail.com>
2022-07-11 12:46:59 -0300

    Adicionando conteúdo introdutório de JS

---
+++ Modulos/ModuloII-HTML-CSS3-JS/4-IntroducaoAoJavaScript/2-RecursosBasicosDaLinguagem/1-script.js
@@ -0,0 +1,10 @@
+// var y = 0;
+
+/*var x = 1;*/
+
+// var preco = 2;
+var desconto = 0.2;
+const PRECO = 2; // Boa prática de programação é constantes serem em CAIXA ALTA
+var total = PRECO - desconto;
+
+console.log(total);
---
+++ Modulos/ModuloII-HTML-CSS3-JS/4-IntroducaoAoJavaScript/2-RecursosBasicosDaLinguagem/2-funcoes.js
@@ -0,0 +1,6 @@
+function soma(a, b){
+    return a + b; // 8 
+}
+
+soma(3, 5); // 8 
+console.log(soma(3,5));
