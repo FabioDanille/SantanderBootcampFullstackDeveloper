25fe4075cf358cc25d333ea857a9c16af0d3449d
Fabio <fabiodanille@hotmail.com>
2022-07-12 13:51:50 -0300

    Corrigindo aula sobre Map

--- Modulos/ModuloII-HTML-CSS3-JS/9-Map-FilterEReduce/1-Map/2-Map-vs-forEach.js
+++ Modulos/ModuloII-HTML-CSS3-JS/9-Map-FilterEReduce/1-Map/2-Map-vs-forEach.js
@@ -1,11 +1,11 @@
 // Usando Map
-const array [1, 2, 3, 4, 5];
+const array = [1, 2, 3, 4, 5];
 
 array.map((item) => item * 2); // retorno: [2, 4, 6, 8, 10] (vai existir os 2 arrays)
 
 
 // Usando forEach
-const array [1, 2, 3, 4, 5];
+const array = [1, 2, 3, 4, 5];
 
 array.forEach((item) => item * 2); // retorno : undefined (Requer uma constante para pegar o valor)
 
